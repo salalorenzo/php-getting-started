9e56df8a7e248116d88c72494fedf569c5c386f6
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-11-06 22:05:44 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -32,4 +32,18 @@ $app->get('/', function() use($app) {
   return str_repeat('Hello', getenv('TIMES'));
 });
 
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
+
 $app->run();
