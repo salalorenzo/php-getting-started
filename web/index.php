c88f3a7a60b8efb63d2d9ba974893505a3461142
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-11-17 10:13:08 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,12 @@
 <?php
     setcookie('c', isset($_COOKIE['c']) ? $_COOKIE['c']++ : 1);
     $contaVisite = $_COOKIE['c'];
+
+    if ($contaVisite == 1): 
+        E' la prima volta!
+    else:
+        Totale visite: <?= $_COOKIE['c'] ?> times. 
+    endif;
 ?>
 
 
@@ -9,10 +15,6 @@
         <title>Contatore accessi alla pagina</title> 
     </head> 
     <body> 
-        <?if ($contaVisite == 1): ?>
-            E' la prima volta!
-        <?else:?> 
-            Totale visite: <?= $_COOKIE['c'] ?> times. 
-        <?endif;?>
+
     </body> 
 </html>
