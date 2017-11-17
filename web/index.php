db85b9483a636934f22ba43e03751ac18ecf4701
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-11-17 09:51:42 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,24 @@
-<?php
-  echo "Hello World!";
-  echo "PHP is so easy!";
-?>
+<html> 
+    <head> 
+        <title>Count Page Access</title> 
+   </head> 
+  <body> 
+<?php 
+
+    if (!isset($_COOKIE['count']))
+    {
+        ?> 
+Welcome! This is the first time you have viewed this page. 
+<?php 
+        $cookie = 1;
+        setcookie("count", $cookie);
+    }
+    else
+    {
+        $cookie = $_COOKIE['count']++;
+        setcookie("count", $cookie);
+        ?> 
+You have viewed this page <?= $_COOKIE['count'] ?> times. 
+<?php  }// end else  ?> 
+   </body> 
+</html>
