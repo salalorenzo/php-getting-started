99633a05cb50532c106e655dea901f217ee7d07c
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-11-17 09:54:26 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,24 +1,16 @@
+<?php
+    setcookie('count', isset($_COOKIE['count']) ? $_COOKIE['count']++ : 1);
+    $visitCount = $_COOKIE['count'];
+?>
 <html> 
     <head> 
         <title>Count Page Access</title> 
-   </head> 
-  <body> 
-<?php 
-
-    if (!isset($_COOKIE['count']))
-    {
-        ?> 
-Welcome! This is the first time you have viewed this page. 
-<?php 
-        $cookie = 1;
-        setcookie("count", $cookie);
-    }
-    else
-    {
-        $cookie = $_COOKIE['count']++;
-        setcookie("count", $cookie);
-        ?> 
-You have viewed this page <?= $_COOKIE['count'] ?> times. 
-<?php  }// end else  ?> 
-   </body> 
+    </head> 
+    <body> 
+        <?if ($visitCount == 1): ?>
+            Welcome! This is the first time you have viewed this page. 
+        <?else:?> 
+            You have viewed this page <?= $_COOKIE['count'] ?> times. 
+        <?endif;?>
+    </body> 
 </html>
