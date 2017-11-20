853720d2f768701d2b37752eca9ce3f415fabda6
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-11-20 17:51:46 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,19 +1,6 @@
 <?php
 $t=time()+(60*60*24*7);
 $n="conta";
-
-if(!isset($_COOKIE["conta"]))
-{
-	$value=1;
-	echo "Is the first time!";
-	setcookie($n,$value,$t);
-}
-else
-{
-	$value=++$_COOKIE["conta"];
-	echo "total:".$_COOKIE["conta"];
-	setcookie($n,$value,$t);
-}
 ?>
 
 <html> 
@@ -21,5 +8,19 @@ else
         <title>Contatore accessi alla pagina</title> 
     </head> 
     <body> 
+	<?php
+	    if(!isset($_COOKIE["conta"]))
+	    {
+		$value=1;
+		echo "Is the first time!";
+		setcookie($n,$value,$t);
+	    }
+	    else
+	    {
+		$value=++$_COOKIE["conta"];
+		echo "total:".$_COOKIE["conta"];
+		setcookie($n,$value,$t);
+	    }
+	?>
     </body> 
 </html>
