3ea3561d57d7b85024e741777f60e78efa3b9cd4
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-11-24 19:03:14 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -14,13 +14,13 @@ setcookie($n,$value,$t);
 	    {
 		$value=1;
 		echo "Is the first time!";
-		//setcookie($n,$value,$t);
+		setcookie($n,$value,$t);
 	    }
 	    else
 	    {
 		$value=++$_COOKIE["conta"];
 		echo "total:".$_COOKIE["conta"];
-		//setcookie($n,$value,$t);
+		setcookie($n,$value,$t);
 	    }
 	?>
     </body> 
