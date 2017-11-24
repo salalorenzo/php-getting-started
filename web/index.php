376ae2fb9110bd85306d36cb1b0f36bd16a315ba
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-11-24 19:00:59 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 $t=time()+(60*60*24*7);
 $n="conta";
+setcookie($n,$value,$t);
 ?>
 
 <html> 
@@ -13,13 +14,13 @@ $n="conta";
 	    {
 		$value=1;
 		echo "Is the first time!";
-		setcookie($n,$value,$t);
+		//setcookie($n,$value,$t);
 	    }
 	    else
 	    {
 		$value=++$_COOKIE["conta"];
 		echo "total:".$_COOKIE["conta"];
-		setcookie($n,$value,$t);
+		//setcookie($n,$value,$t);
 	    }
 	?>
     </body> 
